12e88b35e0dcce9d8ef78587fd5d9059d813d1a6
Balabanovi <erybaz@wp.pl>
2022-09-26 20:21:17 +0200

    Add project files.

---
+++ GuessTheNumber2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace GuessTheNumber2 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            initGame();
+
+            Console.ReadLine(); 
+        }
+
+        static void initGame()
+        {
+            Console.WriteLine("The Greatest Guess the Number Game of All Time. PART 2!\nYour goal is to guess the number from 0 to 10.\nWith each successive game, the number range will increase.\nThe game will give you a hint if you fail to guess.");
+            nextGame();
+        }
+
+        static void nextGame()
+        {
+            Console.WriteLine();
+
+            Round newRound = new Round();
+            bool result;
+
+            do
+            {
+                newRound.Attempt();
+                result = newRound.CheckGuess();
+            } while (!result);
+
+            playAgain();
+        }
+
+
+        static void playAgain()
+        {
+            Console.Write("Play again? Y/N: ");
+            char newGame = Convert.ToChar(Console.ReadLine());
+            if (newGame == 'Y' || newGame == 'y')
+            {
+                Console.WriteLine();
+                Console.WriteLine("------------------------");
+                Console.WriteLine();
+
+                nextGame();
+            }
+            else if (newGame == 'N' || newGame == 'n')
+            {
+                Environment.Exit(0);
+            }
+            else
+            {
+                playAgain();
+            }
+        }
+    }
+}
---
+++ GuessTheNumber2/Round.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessTheNumber2
+{
+    class Round
+    {
+        static Random randomNum = new Random();
+        static int winningNum = randomNum.Next(0, 10);
+        public int steps = 0;
+        public int guess;
+        static int roundNum = 0;
+
+        public Round()
+        {
+            roundNum++;
+            winningNum = randomNum.Next(0, GetMaxNumber());
+            steps = 0;
+            guess = -1;
+            Console.WriteLine("[ Game " + roundNum + " ]");
+         //   Console.WriteLine("* Winning number: " + winningNum);
+        }
+
+        public int GetMaxNumber()
+        {
+            return 10 + (10 * (roundNum-1));
+        }
+
+        public void Attempt()
+        {
+            try
+            {
+                Console.Write("Guess the number from 0 to {0}: ", GetMaxNumber());
+                guess = Convert.ToInt32(Console.ReadLine());
+            }
+            catch
+            {
+            }
+            steps++;
+            Console.WriteLine();
+        }
+
+        public bool CheckGuess()
+        {
+            if (guess != winningNum)
+            {
+                if (guess < 0 || guess > GetMaxNumber())
+                {
+                    Console.WriteLine("Wrong guess! Remember that you are supposed to guess a number from 0 to {0}. Try again.", GetMaxNumber());
+                }
+                else if (guess > winningNum)
+                {
+                    Console.WriteLine("Wrong guess! Your number was too high! Try again.");
+                }
+                else if (guess < winningNum)
+                {
+                    Console.WriteLine("Wrong guess! Your number was too low! Try again.");
+                }
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("You won the game!\nIt took you " + steps + " guess(es) to win this time.");
+                return true;
+            }
+        }
+    }
+}
